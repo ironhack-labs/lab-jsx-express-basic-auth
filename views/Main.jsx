24b523dacbb6f16588654bf2b1c88c23230a0b77
Lucia Skudlik <lucia.skudlik@gmail.com>
2020-11-17 18:19:25 +0100

    start lab

---
+++ views/Main.jsx
@@ -0,0 +1,12 @@
+const React = require("react");
+const Layout = require("./Layout");
+
+function Main() {
+  return (
+    <Layout>
+      <img src="./../images/download.jpeg" alt="" />
+    </Layout>
+  );
+}
+
+module.exports = Main;
---
+++ views/Private.jsx
@@ -0,0 +1,12 @@
+const React = require("react");
+const Layout = require("./Layout");
+
+function Private() {
+  return (
+    <Layout>
+      <h1>This page is private</h1>
+    </Layout>
+  );
+}
+
+module.exports = Private;
